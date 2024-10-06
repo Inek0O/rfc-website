3df288fbcdde436c2a21b288c2cb62b14162070d
Inek0O <jujudodoranrange@gmail.com>
2024-10-06 18:21:46 +0200

    Changed the style of the footer + removed old files

--- templates/footer.php
+++ templates/footer.php
@@ -1,21 +1,46 @@
 <footer>
-	<div id="sponso">
-		<b>Nos Sponsors</b>
-		<ul class="cred_list">
-			<li><a class="sponso_link" href="https://www.auvergnerhonealpes.fr/" target = top>Auvergne Rhône Alpes</a></li>
-			<li><a class="sponso_link" href="https://cotiere.ent.auvergnerhonealpes.fr/" target = top>Lycée de la Côtière</a></li>
-		</ul>
-	</div>
-	<div id="designers">
-		<b>Designer</b><br>
-		<ul class="cred_list">
-			<li>Jules Le Roi</li>
-		</ul>
-	</div>
-	<div id="devs">
-		<b>Développeur</b><br>
-		<ul class="cred_list">
-			<li>Jules Le Roi</li>
-		</ul>
+	<div id=footer>
+		<div class="footer_divs" id="index">
+			<ul class="footer_list">
+				<li>
+					<a class="index_link" href="<?=$base?>/">Accueil</a>
+				</li>
+				<li>
+					<a class="index_link" href="<?=$base?>/team">Équipe</a>
+				</li>
+				<li>
+					<a class="index_link" href="<?=$base?>/divisions">Pôles</a>
+				</li>
+				<li>
+					<a class="index_link" href="<?=$base?>/progress">Progrès</a>
+				</li>
+				<li>
+					<a class="index_link" href="<?=$base?>/contact">Contact</a>
+				</li>
+				<li>
+					<a class="index_link" href="<?=$base?>/creds">Crédits</a>
+				</li>
+			</ul>
+		</div>
+		<div class="footer_divs" id="sponsors">
+			<a class="footer_title" href="<?=$base?>/sponsors">
+				Nos Sponsors
+			</a>
+			<div id="sponsor_list">
+				<a href="https://cotiere.ent.auvergnerhonealpes.fr/" class="sponsors_link">
+					<img src="./images/LogoLycée.png" class="sponsor_logo">
+				</a>
+				<a href="https://www.auvergnerhonealpes.fr/" class="sponsors_link">
+					<img src="./images/LogoRegion.png" class="sponsor_logo">
+				</a>
+				<a href="https://www.maxon.net/fr/about-maxon" class="sponsors_link">
+					<img src="./images/Maxon_Logo_Horizontal_Red_White.png" class="sponsor_logo">
+				</a>
+			</div>
+		</div>
+		<div id="other_team_banner">
+			<img class="other_team_element" id="other_team_logo" src="./images/otherTeamLogo.png">
+			<a class="other_team_element" id="other_team_text" href="https://frealc.github.io/ACCUEIL.html"> VISITEZ LE SITE DE L'ÉQUIPE ALLIÉE ! ➔ </a>
+		</div>
 	</div>
 </footer>
---
+++ templates/pages/creds.php
@@ -0,0 +1,12 @@
+<div id="designers">
+    <b>Designer</b><br>
+    <ul class="cred_list">
+        <li>Jules Le Roi</li>
+    </ul>
+</div>
+<div id="devs">
+    <b>Développeur</b><br>
+    <ul class="cred_list">
+        <li>Jules Le Roi</li>
+    </ul>
+</div>
